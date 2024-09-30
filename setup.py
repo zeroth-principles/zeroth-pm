7643572f84d0f1dd66f1e6078fdd8ede3c528038
Ramanuj Lal <ramanujlal@gmail.com>
2024-09-30 10:37:20 -0500

    Renaming 1

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 import os
 
 # dynamically determine the path to Repo2
-local_name = "zeroth-meta"
+local_name = "zeroth-pmstack"
 local_path = os.getcwd().split(os.sep)
 local_path = os.sep.join(local_path[:-1])
 local_path = os.path.join(local_path, local_name)
 
 setup(
-    name="zeroth-quant",
+    name="zeroth-pmstack",
     version="1.0.0",
     description="Quant Repo",
     # python_requires=">=3.5.0",
-    packages = find_packages(where = "zpquant"),
-    package_dir={'': "zpquant"},
+    packages = find_packages(where = "zppmstack"),
+    package_dir={'': "zppmstack"},
     install_requires=[
         f"{local_name} @ file://localhost/{local_path}#egg={local_name}"
     ]
